fb8100ec55b15c2a5bd8b94c62a8d20511d4db8e
udK01 <dominik.muranyi@gmail.com>
2024-03-21 22:10:31 +0000

    Business Section Completed.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,10 @@
-export default function Button() {
-  return <>Button</>;
+export default function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started!
+    </button>
+  );
 }
