5337cc06f5ce2b19214dd49a8bbd6705c65409a4
udK01 <dominik.muranyi@gmail.com>
2024-03-22 22:04:18 +0000

    Cards Completed.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,3 +1,24 @@
+import styles, { layout } from "../style";
+import { card } from "../assets";
+import Button from "./Button";
+
 export default function CardDeal() {
-  return <>CardDeal</>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card dealer <br className="sm:block hidden" /> in a few
+          easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris totor.c Aliquet ultrice sac, ametau.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 }
